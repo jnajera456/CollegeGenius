266c8072656971f097d35274a82a124f7bada0ec
jesseN <jessenajera06@gmail.com>
2025-01-14 10:31:41 -0500

    set default home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,89 +1,38 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./page.module.css";
-import Link from "next/link";
+import CustomButton from "../components/button";
+
+// var isButtonEnabled that is changeable using a checkbox on this page
+// that will be passed down tp the CustomButton to make it disabled/enabled
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>Testing</li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+  const handleChange = () => {
+    setIsButtonEnabled(!isButtonEnabled);
+  };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <Link href="/home" className={styles.secondary}>
-            Go Home
-          </Link>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Hello world, caesar is alive and well!</p>
+      <div>
+        <h5>Donation Box?</h5>
+        <input
+          type="checkbox"
+          checked={isButtonEnabled}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <img
+        src="https://media.newyorker.com/photos/59095bb86552fa0be682d9d0/master/w_2240,c_limit/Monkey-Selfie.jpg"
+        className={styles.monkey}
+      ></img>
+      <CustomButton
+        text="buy him a coffee!"
+        isDisabled={!isButtonEnabled}
+      ></CustomButton>
     </div>
   );
 }
