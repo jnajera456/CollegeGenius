ea9b7c54a03e3fd0748360531a8fb4455da66762
jesseN <jessenajera06@gmail.com>
2025-06-05 16:17:50 -0400

    added style improvements to navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,21 @@
 import styles from "./navbar.module.css";
 
-
 export default function Navbar() {
   return (
-    <div>
-      <a href="/" >Home</a>
-      <a href="/about">About</a>
-      <a href="/features">Features</a>
+    <div className={styles.navbarContent}>
+      <div className={styles.logoSection}>
+      <img
+        className={styles.logo}
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F026%2F757%2F818%2Fnon_2x%2Fgraduation-cap-with-ai-generated-free-png.png&f=1&nofb=1&ipt=62e6fb5f1c49b94046aedbf1e450a6de9d3c0a37e19ce954fce5270a40b56615"
+        alt="Grad Cap"
+      />
+        <a className={styles.siteName} href="/">CollegeGenius</a> 
+      </div>
+      <div className={styles.linkContainer}>
+        <a className={styles.link} href="/" >Home</a>
+        <a className={styles.link} href="/about">About</a>
+        <a className={styles.link} href="/features">Features</a>
+      </div>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 "use client";
 import Navbar from "../components/navbar/navbar";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
-      <h1>Welcome to CollegeGenius</h1>
+      <h1 className={styles.mainTitle}>On the Home Page</h1>
     </main>
   );
 }
