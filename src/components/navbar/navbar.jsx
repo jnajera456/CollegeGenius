5e24dfe374df950ca40e73ccdd7155af9023ee2d
jesseN <jessenajera06@gmail.com>
2025-06-05 14:55:15 -0400

    Restored app to default to start from scratch, with addition of basic navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import styles from "./navbar.module.css";
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <a href="/" >Home</a>
+      <a href="/about">About</a>
+      <a href="/features">Features</a>
+    </div>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+"use client";
+import Navbar from "../components/navbar/navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar></Navbar>
+      <h1>Welcome to CollegeGenius</h1>
+    </main>
+  );
+}
